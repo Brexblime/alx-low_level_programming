f7d6bf76954564c4ee9d525344ef54706df3dd8a
Brexblime <jaouhari.bahaadine@gmail.com>
2023-02-21 13:59:09 -0800

    Write a function that adds two integers and returns the result.
    Prototype: int add(int, int);
    julien@ubuntu:~/$ cat 10-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int n;
    
        n = add(89, 9);
        printf("%d\n", n);
        return (0);
    }
    julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 10-main.c 10-add.c -o 10-add
    julien@ubuntu:~/0x02$ ./10-add
    98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * add - Function
+ * Description: Adds two numbers
+ * @g1: the giving value
+ * @g2: the second giving value
+ * Return: the result.
+ */
+
+int add(int g1, int g2)
+{
+	int result;
+
+	result = g1 + g2;
+
+	return (result);
+
+}
