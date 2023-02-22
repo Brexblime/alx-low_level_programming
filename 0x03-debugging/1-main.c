80369ce848c16153880f88e5579ce0daa7a2813f
Brexblime <jaouhari.bahaadine@gmail.com>
2023-02-22 08:49:00 -0800

    Copy this main file. Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop.
    Don’t add or remove any lines of code, as we will be checking your line count. You are only allowed to comment out existing code.
    You do not have to compile with -Wall -Werror -Wextra -pedantic for this task.
    carrie@ubuntu:/debugging$ cat 1-main.c
    #include <stdio.h>
    
    /**
    * main - causes an infinite loop
    * Return: 0
    */
    
    int main(void)
    {
            int i;
    
            printf("Infinite loop incoming :(\n");
    
            i = 0;
    
            while (i < 10)
            {
                    putchar(i);
            }
    
            printf("Infinite loop avoided! \\o/\n");
    
            return (0);
    }
    carrie@ubuntu:/debugging$
    Your output should look like this:
    
    carrie@ubuntu:/debugging$ gcc -std=gnu89 1-main.c -o 1-main
    carrie@ubuntu:/debugging$ ./1-main
    Infinite loop incoming :(
    Infinite loop avoided! \o/

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - cause an infinite loop
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -14,9 +14,10 @@ int main(void)
 	i = 0;
 
 	/*
+	* Commenting out the code below to avoid infinite loop
 	while (i < 10)
 	{
-		putchar(i);
+			putchar(i);
 	}
 	*/
 
