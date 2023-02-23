523b1453f61e17a699b581b33ed66c6a829af8df
Brexblime <jaouhari.bahaadine@gmail.com>
2023-02-23 12:06:35 -0800

    Write a function that checks for uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise
    FYI: The standard library provides a similar function: isupper. Run man isupper to learn more.
    
    julien@ubuntu:~/0x04$ cat 0-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code.
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char c;
    
        c = A;
        printf("%c: %d\n", c, _isupper(c));
        c = a;
        printf("%c: %d\n", c, _isupper(c));
        return (0);
    }
    julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-isupper.c -o 0-isuper
    julien@ubuntu:~/0x04$ ./0-isuper
    A: 1
    a: 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
- * Retutn: 1 (if c is uppercase) , 0 (if anything else)
-*/
+ * _isupper - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
