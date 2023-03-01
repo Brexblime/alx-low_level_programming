45509555bb3f905524eb2f8bba867306c9a89568
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-01 22:42:02 +0100

    Write a function that reverses the content of an array of integers.
    Prototype: void reverse_array(int *a, int n);
    Where n is the number of elements of the array
    julien@ubuntu:~/0x06$ cat 4-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code
     * @a: an array of integers
     * @n: the number of elements to swap
     *
     * Return: nothing.
     */
    void print_array(int *a, int n)
    {
        int i;
    
        i = 0;
        while (i < n)
        {
            if (i != 0)
            {
                printf(", ");
            }
            printf("%d", a[i]);
            i++;
        }
        printf("\n");
    }
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
    
        print_array(a, sizeof(a) / sizeof(int));
        reverse_array(a, sizeof(a) / sizeof(int));
        print_array(a, sizeof(a) / sizeof(int));
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-rev_array.c -o 4-rev_array
    julien@ubuntu:~/0x06$ ./4-rev_array
    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337
    1337, 1024, 98, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: the array to reverse
+ * @n: the number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
