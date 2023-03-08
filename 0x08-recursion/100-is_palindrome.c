968b5ebd7f51440d07a2fcd7afc1fccfdd97bc09
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-08 18:10:30 +0100

    Write a function that returns 1 if a string is a palindrome and 0 if not.
    Prototype: int is_palindrome(char *s);
    An empty string is a palindrome
    julien@ubuntu:~/0x08. Recursion$ cat 100-main.c
    #include "main.h"
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int r;
    
        r = is_palindrome("level");
        printf("%d\n", r);
        r = is_palindrome("redder");
        printf("%d\n", r);
        r = is_palindrome("test");
        printf("%d\n", r);
        r = is_palindrome("step on no pets");
        printf("%d\n", r);
        return (0);
    }
    julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-is_palindrome.c -o 100-palindrome
    julien@ubuntu:~/0x08. Recursion$ ./100-palindrome
    1
    1
    0
    1

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_palindrome - that returns 1 if a string is a palindrome and 0 if not
+ * @s: the string to be checked
+ * Return: 1 if palindrome or 0 if not
+*/
+
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+	int i = 0;
+
+	while (i < length / 2)
+	i++;
+	{
+		if (s[i] != s[length - 1 - i])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
