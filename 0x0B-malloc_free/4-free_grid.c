8fec858182af96281c62e9e7ef16e959d492672c
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-13 16:34:35 +0100

    Write a function that frees a 2 dimensional grid previously created by your alloc_grid function.
    Prototype: void free_grid(int **grid, int height);
    Note that we will compile with your alloc_grid.c file. Make sure it compiles.
    julien@ubuntu:~/0x0a. malloc, free$ cat 4-main.c
    #include "main.h"
    #include <stdio.h>
    #include <stdlib.h>
    
    /**
     * print_grid - prints a grid of integers
     * @grid: the address of the two dimensional grid
     * @width: width of the grid
     * @height: height of the grid
     *
     * Return: Nothing.
     */
    void print_grid(int **grid, int width, int height)
    {
        int w;
        int h;
    
        h = 0;
        while (h < height)
        {
            w = 0;
            while (w < width)
            {
                printf("%d ", grid[h][w]);
                w++;
            }
            printf("\n");
            h++;
        }
    }
    
    /**
     * main - check the code for ALX School students.
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int **grid;
    
        grid = alloc_grid(6, 4);
        if (grid == NULL)
        {
            return (1);
        }
        print_grid(grid, 6, 4);
        printf("\n");
        grid[0][3] = 98;
        grid[3][4] = 402;
        print_grid(grid, 6, 4);
        free_grid(grid, 4);
        return (0);
    }
    julien@ubuntu:~/0x0a. malloc, free$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 3-alloc_grid.c 4-free_grid.c -o f
    julien@ubuntu:~/0x0a. malloc, free$ valgrind ./f

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * free_grid - Frees a 2 dimensional grid previously created by alloc_grid function.
+ * free_grid - Frees a 2 dimensional grid previously created by alloc_grid fnct
  * @grid: The 2 dimensional grid to be freed.
  * @height: The height of the grid.
  *
  * Return: Nothing.
- */
+*/
 
 void free_grid(int **grid, int height)
 {
@@ -14,7 +14,7 @@ void free_grid(int **grid, int height)
 
 	for (i = 0; i < height; i++)
 	{
-        	free(grid[i]);
+		free(grid[i]);
 	}
 
 	free(grid);
