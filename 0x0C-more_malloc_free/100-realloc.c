3c0005cfaf5b71582db74206c80c21315f173463
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-16 17:52:01 +0100

    Write a function that reallocates a memory block using malloc and free
    Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
    where ptr is a pointer to the memory previously allocated with a call to malloc: malloc(old_size)
    old_size is the size, in bytes, of the allocated space for ptr
    and new_size is the new size, in bytes of the new memory block
    The contents will be copied to the newly allocated space, in the range from the start of ptr up to the minimum of the old and new sizes
    If new_size > old_size, the “added” memory should not be initialized
    If new_size == old_size do not do anything and return ptr
    If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
    If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
    Don’t forget to free ptr when it makes sense
    FYI: The standard library provides a different function: realloc. Run man realloc to learn more.
    
    julien@ubuntu:~/0x0b. more malloc, free$ cat 100-main.c
    #include "main.h"
    #include <stdio.h>
    #include <stdlib.h>
    #include <string.h>
    
    /**
     * simple_print_buffer - prints buffer in hexa
     * @buffer: the address of memory to print
     * @size: the size of the memory to print
     *
     * Return: Nothing.
     */
    void simple_print_buffer(char *buffer, unsigned int size)
    {
        unsigned int i;
    
        i = 0;
        while (i < size)
        {
            if (i % 10)
            {
                printf(" ");
            }
            if (!(i % 10) && i)
            {
                printf("\n");
            }
            printf("0x%02x", buffer[i]);
            i++;
        }
        printf("\n");
    }
    
    /**
     * main - check the code for
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *p;
        int i;
    
        p = malloc(sizeof(char) * 10);
        p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
        i = 0;
        while (i < 98)
        {
            p[i++] = 98;
        }
        simple_print_buffer(p, 98);
        free(p);
        return (0);
    }
    julien@ubuntu:~/0x0b. more malloc, free$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-realloc.c -o 100-realloc
    julien@ubuntu:~/0x0b. more malloc, free$ ./100-realloc
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62
    0x62 0x62 0x62 0x62 0x62 0x62 0x62 0x62

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with malloc
- * @old_size: size of the allocated space for ptr
- * @new_size: new size of the memory block
+ * _realloc - Reallocates a memory block using malloc and free
+ * @ptr: Pointer to the memory previously allocated with a call to malloc
+ * @old_size: The size, in bytes, of the allocated space for ptr
+ * @new_size: The new size, in bytes, of the new memory block
  *
- * Return: pointer to the reallocated memory block
+ * Return: Pointer to the reallocated memory block
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	char *new_ptr;
+	unsigned int i;
 
 	if (new_size == 0)
 	{
@@ -24,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	}
 
-	if (new_size <= old_size)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}
@@ -35,7 +36,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	memcpy(new_ptr, ptr, old_size);
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		new_ptr[i] = ((char *)ptr)[i];
+	}
+
 	free(ptr);
+
 	return (new_ptr);
 }
