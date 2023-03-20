50e8f9351d50d3a34a9f96134181920a440b6ce7
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-20 12:40:19 +0000

    Write a program that prints the name of the file it was compiled from, followed by a new line.
    You are allowed to use the standard library
    julien@ubuntu:~/0x0c. macro, structures$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c -o c
    julien@ubuntu:~/0x0c. macro, structures$ ./c
    2-main.c
    julien@ubuntu:~/0x0c. macro, structures$ cp 2-main.c 02-main.c
    julien@ubuntu:~/0x0c. macro, structures$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 02-main.c -o cc
    julien@ubuntu:~/0x0c. macro, structures$ ./cc
    02-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - print the name of the file it was compiled from, followed by new line
+ * Return: 0 (Success)
+*/
+
+int main(void)
 {
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 	return (0);
 }
