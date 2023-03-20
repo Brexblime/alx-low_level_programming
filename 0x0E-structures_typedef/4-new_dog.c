33b3c96d172ca82fbc316d429607d8b9ea9fa18f
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-20 19:15:49 +0000

    Write a function that creates a new dog.
    Prototype: dog_t *new_dog(char *name, float age, char *owner);
    You have to store a copy of name and owner
    Return NULL if the function fails
    julien@ubuntu:~/0x0d. structures, typedef$ cat 4-main.c
    #include <stdio.h>
    #include "dog.h"
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        dog_t *my_dog;
    
        my_dog = new_dog("Poppy", 3.5, "Bob");
        printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
        return (0);
    }
    julien@ubuntu:~/0x0d. structures, typedef$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-new_dog.c -o e
    julien@ubuntu:~/0x0d. structures, typedef$ ./e
    My name is Poppy, and I am 3.5 :) - Woof!

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
-#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 
 /**
  * new_dog - creates a new struct of type dog
@@ -12,16 +12,26 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *d;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
-	dog->name = strdup(name);
-	dog->age = age;
-	dog->owner = strdup(owner);
-
-	return (dog);
+	d->name = strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->age = age;
+	d->owner = strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	return (d);
 }
