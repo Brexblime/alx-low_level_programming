ae8db3cb7a8f1a6e8e2dac103b403c909e80e864
BAHAADINE JAOUHARI <101993517+Brexblime@users.noreply.github.com>
2023-03-22 18:43:25 +0000

    Write a program that performs simple operations.
    You are allowed to use the standard library
    Usage: calc num1 operator num2
    You can assume num1 and num2 are integers, so use the atoi function to convert them from the string input to int
    operator is one of the following:
    +: addition
    -: subtraction
    *: multiplication
    /: division
    %: modulo
    The program prints the result of the operation, followed by a new line
    You can assume that the result of all operations can be stored in an int
    if the number of arguments is wrong, print Error, followed by a new line, and exit with the status 98
    if the operator is none of the above, print Error, followed by a new line, and exit with the status 99
    if the user tries to divide (/ or %) by 0, print Error, followed by a new line, and exit with the status 100
    This task requires that you create four different files.     julien@ubuntu:~/0x0e. Function pointers$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-op_functions.c 3-get_op_func.c -o calc
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 1 + 1
    2
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 97 + 1
    98
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 / 10
    102
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 * 98
    100352
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 * 98
    Error
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 - 98
    926
    julien@ubuntu:~/0x0e. Function pointers$ ./calc 1024 % 98
    44
    julien@ubuntu:~/0x0e. Function pointers$

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,8 @@
-#ifndef _3_CALC_H_
-#define _3_CALC_H_
+#ifndef CALC_H
+#define CALC_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct op - Struct op
@@ -10,17 +10,19 @@
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
 
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
+
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,23 +3,24 @@
 #include <stdlib.h>
 
 /**
- * op_add - returns the sum of a and b
- * @a: first integer
- * @b: second integer
- * Return: sum of a and b
- */
+ * op_add - add to number
+ * @a: number 1
+ * @b: number 2
+ * Return: sum of two number
+*/
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+
 /**
- * op_sub - returns the difference of a and b
- * @a: first integer
- * @b: second integer
- * Return: difference of a and b
- */
+ * op_sub - sub two number
+ * @a: number 1
+ * @b: number 2
+ * Return: sub of two number
+*/
 
 int op_sub(int a, int b)
 {
@@ -27,11 +28,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - returns the product of a and b
- * @a: first integer
- * @b: second integer
- * Return: product of a and b
- */
+ * op_mul - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: multiply of two number
+*/
 
 int op_mul(int a, int b)
 {
@@ -39,25 +40,35 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - returns the result of the division of a by b
- * @a: first integer
- * @b: second integer
- * Return: result of the division of a by b
- */
+ * op_div - divide two number
+ * @a: number 1
+ * @b: number 2
+ * Return: divition of two numbers
+*/
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - returns the remainder of the division of a by b
- * @a: first integer
- * @b: second integer
- * Return: remainder of the division of a by b
- */
+ * op_mod - modulize two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: modulo of two numbers
+*/
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
