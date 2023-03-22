7ac9012b8b78971abf8e50020717d651aa4f571c
BAHAADINE JAOUHARI <101993517+Brexblime@users.noreply.github.com>
2023-03-22 16:48:19 +0000

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: the name to be printed
+ * @f: the function to be recalled
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	void print(char *)
+	{
+		printf(char *);
+	}
+
+	int main()
+	{
+		void (*f)(char *);
+			f = print;
+			f(char *);
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,23 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+#endif
