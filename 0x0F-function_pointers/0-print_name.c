238eaf9bd7e46737d3c43ef258a26a0cce698d10
BAHAADINE JAOUHARI <101993517+Brexblime@users.noreply.github.com>
2023-03-22 17:02:48 +0000

    Write a function that prints a name.
    Prototype: void print_name(char *name, void (*f)(char *));
    julien@ubuntu:~/0x0e. Function pointers$ cat 0-main.c
    #include <stdio.h>
    #include "function_pointers.h"
    
    /**
     * print_name_as_is - prints a name as is
     * @name: name of the person
     *
     * Return: Nothing.
     */
    void print_name_as_is(char *name)
    {
        printf("Hello, my name is %s\n", name);
    }
    
    /**
     * print_name_uppercase - print a name in uppercase
     * @name: name of the person
     *
     * Return: Nothing.
     */
    void print_name_uppercase(char *name)
    {
        unsigned int i;
    
        printf("Hello, my uppercase name is ");
        i = 0;
        while (name[i])
        {
            if (name[i] >= a && name[i] <= z)
            {
                putchar(name[i] + A - a);
            }
            else
            {
                putchar(name[i]);
            }
            i++;
        }
    }
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        print_name("Bob", print_name_as_is);
        print_name("Bob Dylan", print_name_uppercase);
        printf("\n");
        return (0);
    }
    julien@ubuntu:~/0x0e. Function pointers$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_name.c -o a
    julien@ubuntu:~/0x0e. Function pointers$ ./a
    Hello, my name is Bob
    Hello, my uppercase name is BOB DYLAN

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,20 +3,10 @@
 /**
  * print_name - a function that prints a name
  * @name: the name to be printed
- * @f: the function to be recalled
+ * @f: the function to use to print name
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	void print(char *)
-	{
-		printf(char *);
-	}
-
-	int main()
-	{
-		void (*f)(char *);
-			f = print;
-			f(char *);
-	}
+	f(name);
 }
