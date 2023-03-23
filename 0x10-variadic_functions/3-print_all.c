b5109b89913670a63f5365349ca21923d375c8c3
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-23 18:59:52 +0000

    Write a function that prints anything.
    Prototype: void print_all(const char * const format, ...);
    where format is a list of types of arguments passed to the function
    c: char
    i: integer
    f: float
    s: char * (if the string is NULL, print (nil) instead
    any other char should be ignored
    see example
    You are not allowed to use for, goto, ternary operator, else, do ... while
    You can use a maximum of
    2 while loops
    2 if
    You can declare a maximum of 9 variables
    You are allowed to use printf
    Print a new line at the end of your function
    julien@ubuntu:~/0x0f. Variadic functions$ cat 3-main.c
    #include "variadic_functions.h"
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        print_all("ceis", B, 3, "stSchool");
        return (0);
    }
    julien@ubuntu:~/0x0f. Variadic functions$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-print_all.c -o d
    julien@ubuntu:~/0x0f. Variadic functions$ ./d
    B, 3, stSchool

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ *
+ * Description: c: char, i: integer, f: float, s: char *
+ *              any other char should be ignored
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					str = "(nil)";
+				}
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+
+		if (format[i + 1])
+			printf(", ");
+
+		i++;
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
