69e5e54abf34f9dec7df6b74c23c0f43192d0ed6
Brexblime <jaouhari.bahaadine@gmail.com>
2023-04-03 15:30:27 +0000

    Write a function that prints all the elements of a listint_t list.
    Prototype: size_t print_listint(const listint_t *h);
    Return: the number of nodes
    Format: see example
    You are allowed to use printf
    julien@ubuntu:~/0x13. More singly linked lists$ cat 0-main.c
    #include <stdlib.h>
    #include <string.h>
    #include <stdio.h>
    #include "lists.h"
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        listint_t *head;
        listint_t *new;
        listint_t hello = {8, NULL};
        size_t n;
    
        head = &hello;
        new = malloc(sizeof(listint_t));
        if (new == NULL)
        {
            printf("Error\n");
            return (1);
        }
        new->n = 9;
        new->next = head;
        head = new;
        n = print_listint(head);
        printf("-> %lu elements\n", n);
        free(new);
        return (0);
    }
    julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_listint.c -o a
    julien@ubuntu:~/0x13. More singly linked lists$ ./a
    9
    8
    -> 2 elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all the elements of a listint_t list
+ * @h: pointer to the head of the linked list
+ * Return: the number of nodes in the list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
