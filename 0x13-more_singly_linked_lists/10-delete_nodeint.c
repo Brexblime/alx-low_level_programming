b00837397186007c740f0997edfde2dbbb7ddd3c
Brexblime <jaouhari.bahaadine@gmail.com>
2023-04-03 18:57:16 +0000

    julien@ubuntu:~/0x13. More singly linked lists$ cat 10-main.c #include <stdlib.h> #include <string.h> #include <stdio.h> #include "lists.h"
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        listint_t *head;
    
        head = NULL;
        add_nodeint_end(&head, 0);
        add_nodeint_end(&head, 1);
        add_nodeint_end(&head, 2);
        add_nodeint_end(&head, 3);
        add_nodeint_end(&head, 4);
        add_nodeint_end(&head, 98);
        add_nodeint_end(&head, 402);
        add_nodeint_end(&head, 1024);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 5);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        printf("-----------------\n");
        delete_nodeint_at_index(&head, 0);
        print_listint(head);
        return (0);
    }
    julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 10-main.c 3-add_nodeint_end.c 0-print_listint.c 5-free_listint2.c 10-delete_nodeint.c -o k
    julien@ubuntu:~/0x13. More singly linked lists$ valgrind ./k

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a listint_t linked list
+ * @head: pointer to pointer to the first element of the list
+ * @index: index of the node to delete
+ * Return: 1 if successful, -1 if failed
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp, *prev;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	prev = *head;
+	temp = (*head)->next;
+	while (temp != NULL)
+	{
+		if (i == index)
+		{
+			prev->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		prev = temp;
+		temp = temp->next;
+		i++;
+	}
+	return (-1);
+}
