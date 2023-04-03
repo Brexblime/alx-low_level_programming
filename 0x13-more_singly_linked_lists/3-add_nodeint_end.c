9c1bb47e39928931d29051ae458cf105c643513d
Brexblime <jaouhari.bahaadine@gmail.com>
2023-04-03 18:03:26 +0000

    Write a function that adds a new node at the end of a listint_t list.
    Prototype: listint_t *add_nodeint_end(listint_t **head, const int n);
    Return: the address of the new element, or NULL if it failed
    julien@ubuntu:~/0x13. More singly linked lists$ cat 3-main.c
    #include <stdlib.h>
    #include <string.h>
    #include <stdio.h>
    #include "lists.h"
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        listint_t *head;
    
        head = NULL;
        add_nodeint_end(&head, 0);
        add_nodeint_end(&head, 1);
        add_nodeint_end(&head, 2);
        add_nodeint_end(&head, 3);
        add_nodeint_end(&head, 4);
        add_nodeint_end(&head, 98);
        add_nodeint_end(&head, 402);
        add_nodeint_end(&head, 1024);
        print_listint(head);
        return (0);
    }
    julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-add_nodeint_end.c 0-print_listint.c -o d
    julien@ubuntu:~/0x13. More singly linked lists$ ./d
    0
    1
    2
    3
    4
    98
    402
    1024

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,14 +2,36 @@
 
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: points to the head of the list
- * @n : integer to be stored in the new node
+ * @head: pointer to the head of the linked list
+ * @n: value of the new node to be added
  * Return: the address of the new element, or NULL if it failed
 */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *ptr, *temp;
 
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
-	return (n);
+	ptr->n = n;
+	ptr->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = ptr;
+		return (ptr);
+	}
+
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = ptr;
+
+	return (ptr);
 }
