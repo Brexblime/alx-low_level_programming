0d2fea226f8d7b737c5c344d29215e28b5d29c88
Brexblime <jaouhari.bahaadine@gmail.com>
2023-04-03 17:47:48 +0000

    Write a function that adds a new node at the beginning of a listint_t list.
    Prototype: listint_t *add_nodeint(listint_t **head, const int n);
    Return: the address of the new element, or NULL if it failed
    julien@ubuntu:~/0x13. More singly linked lists$ cat 2-main.c
    #include <stdlib.h>
    #include <string.h>
    #include <stdio.h>
    #include "lists.h"
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        listint_t *head;
    
        head = NULL;
        add_nodeint(&head, 0);
        add_nodeint(&head, 1);
        add_nodeint(&head, 2);
        add_nodeint(&head, 3);
        add_nodeint(&head, 4);
        add_nodeint(&head, 98);
        add_nodeint(&head, 402);
        add_nodeint(&head, 1024);
        print_listint(head);
        return (0);
    }
    julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-add_nodeint.c 0-print_listint.c -o c
    julien@ubuntu:~/0x13. More singly linked lists$ ./c
    1024
    402
    98
    4
    3
    2
    1
    0

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head : points to the adress of the first node
- * @n: the new node to be added
+ * @n: integer to be stored in the new node
  * Return: the address of the new element, or NULL if it failed
 */
 
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: points to the head of the list
+ * @n : integer to be stored in the new node
+ * Return: the address of the new element, or NULL if it failed
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+
+	return (n);
+}
