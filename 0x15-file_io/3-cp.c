d22a20a4c09bf074d23c550c308dc83d0c8e7e28
Brexblime <jaouhari.bahaadine@gmail.com>
2023-04-09 05:08:14 +0000

    julien@ubuntu:~/0x15. File descriptors and permissions$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-cp.c -o cp julien@ubuntu:~/0x15. File descriptors and permissions$ cat incitatous Why you should think twice before putting pictures on social media. (What you always wanted to know about @Incitatous) #PrivacyAware http://imgur.com/a/Mq1tc julien@ubuntu:~/0x15. File descriptors and permissions$ ./cp incitatous Incitatous julien@ubuntu:~/0x15. File descriptors and permissions$ ls -l Incitatous -rw-rw-r-- 1 julien julien 158 Dec  3 15:39 Incitatous julien@ubuntu:~/0x15. File descriptors and permissions$ cat Incitatous Why you should think twice before putting pictures on social media. (What you always wanted to know about @Incitatous) #PrivacyAware http://imgur.com/a/Mq1tc julien@ubuntu:~/0x15. File descriptors and permissions$

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,37 +12,37 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	/* Check for correct number of arguments */
-	if (argc != 3) {
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);}
+	if (argc != 3)
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
 	/* Open source file for reading */
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1) {
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);}
+	if (fd_from == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	/* Open destination file for writing */
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1) {
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);}
+	if (fd_to == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
 	/* Copy data from source file to destination file */
 	do {
 		rd = read(fd_from, buf, 1024);
-		if (rd == -1) {
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);}
+		if (rd == -1)
+		{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
 		wr = write(fd_to, buf, rd);
-		if (wr == -1) {
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);}
+		if (wr == -1)
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99); }
 	} while (rd == 1024);
 	/* Close file descriptors */
-	if (fd_from == -1) {
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);}
-	if (fd_to == -1) {
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);}
+	if (fd_from == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100); }
+	if (fd_to == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100); }
 
 	return (0);
 }
