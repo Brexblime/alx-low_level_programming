103c8d9fd8021482cf2dbf8d49bd4df77769c18e
Brexblime <jaouhari.bahaadine@gmail.com>
2023-05-18 19:50:46 +0100

    Write a function that inserts a new node at a given position.
    Prototype: dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
    where idx is the index of the list where the new node should be added. Index starts at 0
    Returns: the address of the new node, or NULL if it failed
    if it is not possible to add the new node at index idx, do not add the new node and return NULL
    Your files 2-add_dnodeint.c and 3-add_dnodeint_end.c will be compiled during the correction
    
    julien@ubuntu:~/0x17. Doubly linked lists$ cat 7-main.c
    #include <stdlib.h>
    #include <string.h>
    #include <stdio.h>
    #include "lists.h"
    
    /**
     * main - check the code
     *
     * Return: Always EXIT_SUCCESS.
     */
    int main(void)
    {
        dlistint_t *head;
    
        head = NULL;
        add_dnodeint_end(&head, 0);
        add_dnodeint_end(&head, 1);
        add_dnodeint_end(&head, 2);
        add_dnodeint_end(&head, 3);
        add_dnodeint_end(&head, 4);
        add_dnodeint_end(&head, 98);
        add_dnodeint_end(&head, 402);
        add_dnodeint_end(&head, 1024);
        print_dlistint(head);
        printf("-----------------\n");
        insert_dnodeint_at_index(&head, 5, 4096);
        print_dlistint(head);
        free_dlistint(head);
        head = NULL;
        return (EXIT_SUCCESS);
    }
    julien@ubuntu:~/0x17. Doubly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-main.c 2-add_dnodeint.c 3-add_dnodeint_end.c 0-print_dlistint.c 4-free_dlistint.c 7-insert_dnodeint.c -o j
    julien@ubuntu:~/0x17. Doubly linked lists$ ./j
    0
    1
    2
    3
    4
    98
    402
    1024
    -----------------
    0
    1
    2
    3
    4
    4096
    98
    402
    1024

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,34 +17,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	if (idx == 0)
 	{
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (current == NULL)
 		{
-			return NULL;
+			return (NULL);
 		}
 		current = current->next;
 	}
 
 	if (current == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	if (current->next == NULL)
 	{
-		return add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_node->n = n;
 	new_node->prev = current;
@@ -52,5 +52,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current->next->prev = new_node;
 	current->next = new_node;
 
-	return new_node;
+	return (new_node);
 }
