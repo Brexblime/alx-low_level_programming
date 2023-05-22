fb41d4113eacf96650f6a9d299e7f6d904de13c5
Brexblime <jaouhari.bahaadine@gmail.com>
2023-05-22 16:59:42 +0100

    I know, you’re missing C when coding in Python. So let’s fix that!
    Create a dynamic library that contains C functions that can be called from Python. See example for more detail.
    
    julien@ubuntu:~/0x18$ cat 100-tests.py
    import random
    import ctypes
    
    cops = ctypes.CDLL(./100-operations.so)
    a = random.randint(-111, 111)
    b = random.randint(-111, 111)
    print("{} + {} = {}".format(a, b, cops.add(a, b)))
    print("{} - {} = {}".format(a, b, cops.sub(a, b)))
    print("{} x {} = {}".format(a, b, cops.mul(a, b)))
    print("{} / {} = {}".format(a, b, cops.div(a, b)))
    print("{} % {} = {}".format(a, b, cops.mod(a, b)))
    julien@ubuntu:~/0x16. Doubly linked lists$ python3 100-tests.py
    66 + -76 = -10
    66 - -76 = 142
    66 x -76 = -5016
    66 / -76 = 0
    66 % -76 = 66
    julien@ubuntu:~/0x18$ python3 100-tests.py
    -34 + -57 = -91
    -34 - -57 = 23
    -34 x -57 = 1938
    -34 / -57 = 0
    -34 % -57 = -34
    julien@ubuntu:~/0x18$ python3 100-tests.py
    -5 + -72 = -77
    -5 - -72 = 67
    -5 x -72 = 360
    -5 / -72 = 0
    -5 % -72 = -5
    julien@ubuntu:~/0x18$ python3 100-tests.py
    39 + -62 = -23
    39 - -62 = 101
    39 x -62 = -2418
    39 / -62 = 0
    39 % -62 = 39

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subs two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mul two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mod two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
